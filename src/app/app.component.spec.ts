7bf1640f70cb2fa9f8958e04d01d681503dafbcf
Alyson Gabriel Matos dos Santos <alysongabriel07@gmail.com>
2025-06-26 22:00:58 -0300

    [Creating Git]

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'InkRaven' title`, () => {
+  it(`should have the 'minha-aplicacao' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('InkRaven');
+  
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, InkRaven');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, minha-aplicacao');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'InkRaven';
+  nomeDisciplina = '';
+  professor = '';
+  dataProva = '';
+  duracao = '';
+
+  embaralharQuestoes = false;
+  embaralharAlternativas = false;
+  gerarVersoes = false;
+
+  questao = '';
+  questoes: string[] = [];
+
+  incluirGabarito = false;
+  numerarPaginas = false;
+
+  formatoSaida = 'pdf';
+
+  adicionarQuestao() {
+    if (this.questao.trim()) {
+      this.questoes.push(this.questao.trim());
+      this.questao = '';
+    }
+  }
+
+  gerarProvas() {
+    console.log('Gerando prova com os seguintes dados:', {
+      nomeDisciplina: this.nomeDisciplina,
+      professor: this.professor,
+      dataProva: this.dataProva,
+      duracao: this.duracao,
+      embaralharQuestoes: this.embaralharQuestoes,
+      embaralharAlternativas: this.embaralharAlternativas,
+      gerarVersoes: this.gerarVersoes,
+      questoes: this.questoes,
+      incluirGabarito: this.incluirGabarito,
+      numerarPaginas: this.numerarPaginas,
+      formatoSaida: this.formatoSaida,
+    });
+  }
 }
