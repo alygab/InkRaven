5aef937fd7caeed7056fa744cf0bac23849f1e4d
Alyson Gabriel Matos dos Santos <alysongabriel07@gmail.com>
2025-06-26 23:04:24 -0300

    [margin 0]

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'minha-aplicacao' title`, () => {
+  it(`should have the 'InkRaven' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
   
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, minha-aplicacao');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, InkRaven');
   });
 });
